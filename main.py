ebc689a0533359965c6b372c911bce08059626d4
Kartik Narayan Sahoo <kartik.sahoo26@gmail.com>
2022-03-04 21:00:29 +0530

    deleted the code in home page and added SVR option for modelling

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.preprocessing import StandardScaler
 
 st.write("""
 # Car Price Prediction
@@ -44,14 +45,18 @@ if rad == "Home":
             reg = LinearRegression()
         elif reg_name == 'Decision Tree Regression':
             reg = DecisionTreeRegressor()
+        elif reg_name == 'SVR(Support Vector Regression)':
+            reg = SVR()
         elif reg_name == 'Random Forest Regression':
-            reg = RandomForestRegressor(n_estimators=100)
+            esti = st.slider('Select the number of estimators',
+                             min_value=100, max_value=500, value=100)
+            reg = RandomForestRegressor(n_estimators=esti)
         return reg
 
     # Modelling
     st.header("Modelling")
     select_reg = st.selectbox('Select regressor',
-                              ['Linear Regression', 'Decision Tree Regression', 'Random Forest Regression'])
+                              ['Linear Regression', 'SVR(Support Vector Regression)', 'Decision Tree Regression', 'Random Forest Regression'])
 
     st.subheader('Set the parameter')
     split_ratio = st.slider(
@@ -81,12 +86,27 @@ if rad == "Home":
     # # Training the random forest regression model on the training set
 
     regressor = reg_select(select_reg)
-    regressor.fit(x_train, y_train)
 
-    # # Predicting the test set values
+    if(select_reg == 'SVR(Support Vector Regression)'):
+        # feature scaling
+        sc_x = StandardScaler()
+        sc_y = StandardScaler()
+        x_train[:, 18:-1] = sc_x.fit_transform(x_train[:, 18:-1])
+        y_train = sc_y.fit_transform(y_train.reshape(len(y_train), 1))
 
-    y_pred_test = regressor.predict(x_test)
-    y_pred_train = regressor.predict(x_train)
+        regressor.fit(x_train, y_train.ravel())
+        y_pred_test = sc_y.inverse_transform(
+            regressor.predict(sc_x.transform(x_test)))
+        y_pred_train = sc_y.inverse_transform(
+            regressor.predict(x_train))
+        y_train = sc_y.inverse_transform(y_train)
+    else:
+        regressor.fit(x_train, y_train)
+
+        # # Predicting the test set values
+
+        y_pred_test = regressor.predict(x_test)
+        y_pred_train = regressor.predict(x_train)
 
     # # Evaluating the model performance
 
@@ -108,71 +128,6 @@ if rad == "Home":
     st.write('**Error (MSE)**')
     st.info(mean_squared_error(y_test, y_pred_test))
 
-    # st.subheader('Code')
-    code_reg = """
-    # # Importing the libraries
-
-    import numpy as np
-    import pandas as pd
-    import matplotlib.pyplot as plt
-    import seaborn as sns
-
-
-    # # Importing the dataset
-
-    data = pd.read_csv('data.csv')
-    data.head()
-
-    x = data.iloc[:,1:-1].values
-    y = data.iloc[:,-1].values
-
-
-    # # Correlation between different variables
-
-    plt.figure(figsize=(8,8))
-    sns.heatmap(data.corr(),annot=True,cmap='mako',linewidths=.5)
-
-
-    # # Encoding categorical variables
-
-    from sklearn.compose import ColumnTransformer
-    from sklearn.preprocessing import OneHotEncoder
-
-    ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(),[2,3,4,5])], remainder='passthrough')
-    x = np.array(ct.fit_transform(x))
-
-    print(x[:2])
-    print(y[:2])
-
-
-    # # Splitting the dataset into training and test set
-
-    from sklearn.model_selection import train_test_split
-    x_train, x_test, y_train, y_test = train_test_split(x,y, train_size=0.9, random_state=0)
-
-
-    # # Training the random forest regression model on the training set
-
-    from sklearn.ensemble import RandomForestRegressor
-    regressor = RandomForestRegressor(n_estimators=100, random_state=0)
-    regressor.fit(x_train, y_train)
-
-
-    # # Predicting the test set values
-
-    predictions = regressor.predict(x_test)
-
-    np.set_printoptions(precision= 2)
-    np.concatenate((y_test.reshape(len(y_test),1), predictions.reshape(len(predictions),1)),1)
-
-
-    # # Evaluating the model performance
-
-    from sklearn.metrics import r2_score
-    r2_score(y_test, predictions)
-    """
-    # st.code(code_reg, language='python')
-
 if rad == "Dataset (after cleaning)":
     st.write("""
     The dataset was cleaned and units from certain columns, like torque, engine_cc, etc. was removed and only the numerical values were kept
@@ -182,109 +137,108 @@ if rad == "Dataset (after cleaning)":
     st.text("""
     Below is the code used to clean the original dataset, and get the above dataset
     """)
-    code = """
-    import numpy as np
-    import matplotlib.pyplot as plt
-    import pandas as pd
-    import seaborn as sns
+    code = """import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
 
-    dataset = pd.read_csv('Car details v3.csv')
-    dataset.head()  
-    # counting missing values
-    dataset.isnull().sum()
-    # deleting the rows with missing values
-    dataset = dataset.dropna()
-    import re
+dataset = pd.read_csv('Car details v3.csv')
+dataset.head()  
+# counting missing values
+dataset.isnull().sum()
+# deleting the rows with missing values
+dataset = dataset.dropna()
+import re
 
+# Creating new torque_rpm column with only numeric values
 
-    #Creating new torque_rpm column with only numeric values
+torque_list = dataset["torque"].to_list()
+torque_rpm = []
+def extraction_torque(x):
+    for i in x:
+        res = i.replace(",","")
+        temp = [int(s) for s in re.findall(r'\d+', res)]
+        torque_rpm.append(max(temp))
+        
+extraction_torque(torque_list)
+print(torque_rpm[:2])
 
-    torque_list = dataset["torque"].to_list()
-    torque_rpm = []
-    def extraction_torque(x):
-        for i in x:
-            res = i.replace(",","")
-            temp = [int(s) for s in re.findall(r'\d+', res)]
-            torque_rpm.append(max(temp))
-            
-    extraction_torque(torque_list)
-    print(torque_rpm[:2])
+dataset['torque_rpm'] = torque_rpm
+dataset.head(2)
 
-    dataset['torque_rpm'] = torque_rpm
-    dataset.head(2)
+# extracting mileage
+mil_list = dataset['mileage'].to_list()
+mil_kmpl = []
+def extraction_mil(x):
+for item in x:
+    temp = []
+    try:
+    for s in item.split(" "):
+        temp.append(float(s))
+    except:
+    pass
+    mil_kmpl.append(max(temp))
 
-    # extracting mileage
-    mil_list = dataset['mileage'].to_list()
-    mil_kmpl = []
-    def extraction_mil(x):
+extraction_mil(mil_list)
+print(mil_list[:2])
+print(mil_kmpl[:2])
+
+dataset['mil_kmpl'] = mil_kmpl
+dataset.head()
+
+# for engine
+engine_list = dataset['engine'].to_list()
+engine_cc = []
+def extraction_engine(x):
     for item in x:
         temp = []
         try:
-        for s in item.split(" "):
-            temp.append(float(s))
+            for s in item.split(" "):
+                temp.append(float(s))
         except:
-        pass
-        mil_kmpl.append(max(temp))
+            pass
+        engine_cc.append(max(temp))
 
-    extraction_mil(mil_list)
-    print(mil_list[:2])
-    print(mil_kmpl[:2])
+extraction_engine(engine_list)
+print(engine_list[:2])
+print(engine_cc[:2])
 
-    dataset['mil_kmpl'] = mil_kmpl
-    dataset.head()
+dataset['engine_cc'] = engine_cc
+dataset.head(2)
 
-    # for engine
-    engine_list = dataset['engine'].to_list()
-    engine_cc = []
-    def extraction_engine(x):
-        for item in x:
-            temp = []
-            try:
-                for s in item.split(" "):
-                    temp.append(float(s))
-            except:
-                pass
-            engine_cc.append(max(temp))
+# for max power 
+max_power_list = dataset['max_power'].to_list()
+max_power_bhp = []
 
-    extraction_engine(engine_list)
-    print(engine_list[:2])
-    print(engine_cc[:2])
+def extraction_maxpower(x):
+    for item in x:
+        temp = []
+        try:
+            for s in item.split(" "):
+                temp.append(float(s))
+        except:
+            pass
+        max_power_bhp.append(max(temp))
+        
+extraction_maxpower(max_power_list)
+print(max_power_list[:2])
+print(max_power_bhp[:2])
 
-    dataset['engine_cc'] = engine_cc
-    dataset.head(2)
+dataset["max_power_bhp"] = max_power_bhp
+dataset.head()
 
-    # for max power 
-    max_power_list = dataset['max_power'].to_list()
-    max_power_bhp = []
+# so now let us create a new set dataframe with the original text columns deleted
+data_new = dataset.drop(["mileage","engine","max_power","torque"], axis = 1)
+data_new.head()
 
-    def extraction_maxpower(x):
-        for item in x:
-            temp = []
-            try:
-                for s in item.split(" "):
-                    temp.append(float(s))
-            except:
-                pass
-            max_power_bhp.append(max(temp))
-            
-    extraction_maxpower(max_power_list)
-    print(max_power_list[:2])
-    print(max_power_bhp[:2])
 
-    dataset["max_power_bhp"] = max_power_bhp
-    dataset.head()
+df = data_new.pop('selling_price')
+data_new['selling_price'] = df
 
-    # so now let us create a new set dataframe with the original text columns deleted
-    data_new = dataset.drop(["mileage","engine","max_power","torque"], axis = 1)
-    data_new.head()
-
-    
-    df = data_new.pop('selling_price')
-    data_new['selling_price'] = df
-    
-    # exporting the cleaned dataset as csv
-    data_new.to_csv('data.csv', index=False)
+# exporting the cleaned dataset as csv
+data_new.to_csv('data.csv', index=False)
     """
     st.code(code, language='python')
+
 if rad == "Predictor":
     pass
